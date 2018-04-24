9faf8aecce9e31e7cc8e3b0cc6e481723e06732d
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-24 15:45:37 +0300

    FIR: do not build KtLightClasses during supertype resolve

--- compiler/fir/java/build.gradle.kts
+++ compiler/fir/java/build.gradle.kts
@@ -9,7 +9,9 @@ jvmTarget = "1.6"
 dependencies {
     compile(project(":compiler:frontend.common"))
     compile(project(":compiler:frontend.java"))
+    compile(project(":compiler:light-classes"))
     compile(project(":compiler:fir:resolve"))
+    compile(project(":compiler:fir:tree"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core", "annotations") }
 }
--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
@@ -7,8 +7,10 @@ package org.jetbrains.kotlin.fir.java
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.java.symbols.JavaClassSymbol
 import org.jetbrains.kotlin.fir.resolve.AbstractFirSymbolProvider
+import org.jetbrains.kotlin.fir.serviceOrNull
 import org.jetbrains.kotlin.fir.symbols.ConeSymbol
 import org.jetbrains.kotlin.load.java.structure.JavaClass
 import org.jetbrains.kotlin.load.java.structure.classId
@@ -18,21 +20,22 @@ import org.jetbrains.kotlin.resolve.jvm.KotlinJavaPsiFacade
 
 class JavaSymbolProvider(
     val project: Project,
-    private val searchScope: GlobalSearchScope
+    private val searchScope: GlobalSearchScope,
+    private val session: FirSession
 ) : AbstractFirSymbolProvider() {
 
     override fun getSymbolByFqName(classId: ClassId): ConeSymbol? {
         return classCache.lookupCacheOrCalculate(classId) {
             val facade = KotlinJavaPsiFacade.getInstance(project)
-            val foundClass = facade.findClass(classId, searchScope)
-            foundClass?.let { javaClass -> JavaClassSymbol(this, javaClass) }
+            val foundClass: JavaClass? = facade.findClass(classId, searchScope)
+            foundClass?.let { javaClass -> JavaClassSymbol(this, session.serviceOrNull(), javaClass) }
         }
     }
 
     fun getSymbolByJavaClass(javaClass: JavaClass): ConeSymbol? {
         val classId = javaClass.classId ?: error("!")
         return classCache.lookupCacheOrCalculate(classId) {
-            JavaClassSymbol(this, javaClass)
+            JavaClassSymbol(this, session.serviceOrNull(), javaClass)
         }
     }
 
