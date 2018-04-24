813433ab8330bd4113b30472edcafa2d14ff1889
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-24 15:45:37 +0300

    FIR: do not build KtLightClasses during supertype resolve

--- compiler/fir/java/build.gradle.kts
+++ compiler/fir/java/build.gradle.kts
@@ -7,9 +7,11 @@ plugins {
 jvmTarget = "1.6"
 
 dependencies {
+    compile(project(":compiler:light-classes"))
     compile(project(":compiler:frontend.common"))
     compile(project(":compiler:frontend.java"))
     compile(project(":compiler:fir:resolve"))
+    compile(project(":compiler:fir:tree"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core", "annotations") }
 }
