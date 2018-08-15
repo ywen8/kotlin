3df49a4c0e098c39837f7f792e59aa016081db2e
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-08-15 15:47:02 +0300

    [JS BE] KT-22053 Fix constructor delegation of immediate subtype of Error

--- js/js.translator/testData/box/expression/try/exceptionToString.kt
+++ js/js.translator/testData/box/expression/try/exceptionToString.kt
@@ -4,8 +4,23 @@ package foo
 
 class MyException(m: String? = null): Exception(m)
 class MyException2(m: String? = null): Throwable(m)
+
+// KT-22053
+class MyThrowable(message: String?) :  Throwable("through primary: " + message) {
+    public var initOrder = ""
+
+    constructor() : this(message = "secondary") {
+        initOrder += "2"
+    }
+    constructor(i: Int) : this() {
+        initOrder += "3"
+    }
+
+    init { initOrder += "1" }
+}
+
+
 // TODO: add direct inheritors of Throwable:
-// - with secondary constructors
 // - with cause only, in the primary constructor
 
 fun check(e: Throwable, expectedString: String) {
@@ -32,6 +47,19 @@ fun box(): String {
     check(MyException("aaa"), "MyException: aaa")
     check(MyException2(), "MyException2: null")
     check(MyException2("aaa"), "MyException2: aaa")
+
+    // KT-22053
+    val mt1 = MyThrowable("primary")
+    assertEquals(mt1.toString(), "MyThrowable: through primary: primary")
+    assertEquals(mt1.initOrder, "1")
+
+    val mt2 = MyThrowable()
+    assertEquals(mt2.toString(), "MyThrowable: through primary: secondary")
+    assertEquals(mt2.initOrder, "12")
+
+    val mt3 = MyThrowable(1)
+    assertEquals(mt3.toString(), "MyThrowable: through primary: secondary")
+    assertEquals(mt3.initOrder, "123")
 
     return "OK"
 }
