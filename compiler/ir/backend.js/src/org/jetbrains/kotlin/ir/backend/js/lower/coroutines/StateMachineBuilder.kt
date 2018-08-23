1a932c63906b12fe6ba08d02623e9cbd674b6b01
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-08-23 19:56:49 +0300

    [JS IR BE] Workaround empty blocks in state machine builder

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/coroutines/StateMachineBuilder.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/coroutines/StateMachineBuilder.kt
@@ -377,7 +377,9 @@ class StateMachineBuilder(
                 currentBlock = branchBlock
                 branch.result.acceptVoid(this)
 
-                if (currentBlock.statements.last() !is IrContinue) {
+                // TODO: block should not be empty
+                val lastStatement = currentBlock.statements.lastOrNull()
+                if (lastStatement != null && lastStatement !is IrContinue) {
                     if (currentState !== rootState) {
                         doDispatch(exitState)
                     }
@@ -387,7 +389,10 @@ class StateMachineBuilder(
                 currentBlock = elseBlock
             } else {
                 branch.result.acceptVoid(this)
-                if (currentBlock.statements.last() !is IrContinue) {
+
+                // TODO: block should not be empty
+                val lastStatement = currentBlock.statements.lastOrNull()
+                if (lastStatement != null && lastStatement !is IrContinue) {
                     if (currentState !== rootState) {
                         doDispatch(exitState)
                     }
