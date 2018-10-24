eef1eef22010f663059c0855d3ff0c3d3651a1a7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-10-24 15:33:25 +0300

    ++ import problem workaround

--- idea/fir-view/build.gradle.kts
+++ idea/fir-view/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeJars("util", "openapi", "idea", "asm-all", "jdom", "annotations", "trove4j", "guava", rootProject = rootProject) }
-    compileOnly(intellijPluginDep("gradle")) { includeJars("gradle-tooling-api", "gradle", rootProject = rootProject) }
+    compileOnly(intellijPluginDep("gradle")) //{ includeJars("gradle-tooling-api", "gradle", rootProject = rootProject) }
 }
 
 sourceSets {
