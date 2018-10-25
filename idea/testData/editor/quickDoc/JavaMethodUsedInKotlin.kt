b9ef6baf62d589068f95f5791e6a216226384c76
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2018-10-25 11:14:46 +0300

    Fixed QuickDoc testdata

--- idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
+++ idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
@@ -6,4 +6,4 @@ fun ktTest() {
 //INFO: <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>NotNull</code></a></i>&nbsp;
 //INFO: public static&nbsp;<a href="psi_element://java.lang.Object"><code>Object</code></a>[]&nbsp;<b>foo</b>(<a href="psi_element://java.lang.String"><code>String</code></a>&nbsp;param)</pre></div><div class='content'>
 //INFO:        Java Method
-//INFO:      <p></div><table class='sections'><p><tr><td valign='top' class='section'><p><i>Inferred</i> annotations:</td><td><p><i>@org.jetbrains.annotations.Contract(pure = true)</i> <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>org.jetbrains.annotations.NotNull</code></a></i></td></table>
+//INFO:      <p></div><table class='sections'><p><tr><td valign='top' class='section'><p><i>Inferred</i> annotations:</td><td valign='top'><p><i>@org.jetbrains.annotations.Contract(pure = true)</i> <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>org.jetbrains.annotations.NotNull</code></a></i></td></table>
