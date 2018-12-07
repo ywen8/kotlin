21000c73d643e355f5e869ff2d1c5359faaf9a2c
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-12-07 20:31:40 +0300

    Replace inaccurate note with a link to the reference
    Relates to KT-26604

--- core/builtins/native/kotlin/Any.kt
+++ core/builtins/native/kotlin/Any.kt
@@ -29,8 +29,7 @@ public open class Any {
      * * Transitive:  for any non-null reference values x, y, and z, if x.equals(y) returns true and y.equals(z) returns true, then x.equals(z) should return true
      * * Consistent:  for any non-null reference values x and y, multiple invocations of x.equals(y) consistently return true or consistently return false, provided no information used in equals comparisons on the objects is modified.
      *
-     * Note that the `==` operator in Kotlin code is translated into a call to [equals] when objects on both sides of the
-     * operator are not null.
+     * Read more about [equality](https://kotlinlang.org/docs/reference/equality.html) in Kotlin.
      */
     public open operator fun equals(other: Any?): Boolean
 
