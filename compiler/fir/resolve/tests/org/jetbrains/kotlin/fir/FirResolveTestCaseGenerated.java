7db2ecc0f75631668cad388a9933aa1c66c12cf2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-10 11:50:17 +0300

    FIR: extra test for fake recursive type alias

--- compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirResolveTestCaseGenerated.java
+++ compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirResolveTestCaseGenerated.java
@@ -54,6 +54,11 @@ public class FirResolveTestCaseGenerated extends AbstractFirResolveTestCase {
         runTest("compiler/fir/resolve/testData/resolve/fakeRecursiveSupertype.kt");
     }
 
+    @TestMetadata("fakeRecursiveTypealias.kt")
+    public void testFakeRecursiveTypealias() throws Exception {
+        runTest("compiler/fir/resolve/testData/resolve/fakeRecursiveTypealias.kt");
+    }
+
     @TestMetadata("ft.kt")
     public void testFt() throws Exception {
         runTest("compiler/fir/resolve/testData/resolve/ft.kt");
